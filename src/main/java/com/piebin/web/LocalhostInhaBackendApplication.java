e8b8005a40c2357f72d0dc280004439accf2a058
Bin <baebine@naver.com>
2023-07-10 22:28:27 +0900

    Initial commit

---
+++ src/main/java/com/piebin/web/LocalhostInhaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.piebin.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalhostInhaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalhostInhaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piebin/web/LocalhostInhaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piebin.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalhostInhaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
